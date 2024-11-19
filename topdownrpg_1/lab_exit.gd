aa6f18dab552094557eac26b12e84cdeef4a0fd5
Andrei Popescu <andrei69420@yahoo.com>
2024-11-19 10:40:42 +0200

    Add files via upload

---
+++ topdownrpg_1/lab_exit.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+
+func _on_body_exited(body: Node2D) -> void:
+	entered = false
+	pass # Replace with function body.
+
+func _process(delta):
+	if entered :
+		if Input.is_action_just_pressed("interact"):
+			get_tree().change_scene_to_file("res://main.tscn")
